94d9b14e29d76943ae700d80f46959639aff70c7
Krish9998 <55281240+Krish9998@users.noreply.github.com>
2026-02-02 18:26:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("CI Build Successful");
         System.out.println("Krishna");
+        System.out.println("Pandit");
     }
 }
